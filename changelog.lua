715a105da737c2035460c8580b7a32fa21b0836c
Triantafyllo <89344759+Triantafyllo@users.noreply.github.com>
2021-08-22 16:52:40 +0100

    Create changelog.lua

---
+++ changelog.lua
@@ -0,0 +1,12 @@
+local changelog_table = {
+    ["version 1.1.0"] = {
+        "--- What's new?",
+        "There's now a changelog directly within the UI!"
+    },
+    ["version 1.0.0"] = {
+        "--- What's new?",
+        "The script has been created!"
+    }
+}
+
+return changelog_table
