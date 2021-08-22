e9882a4aa4732b2b3465d1fb6b96b3ccc374cefb
Triantafyllo <89344759+Triantafyllo@users.noreply.github.com>
2021-08-22 17:23:38 +0100

    Update changelog.lua

--- changelog.lua
+++ changelog.lua
@@ -1,6 +1,7 @@
 local changelog_table = {
     ["Version 1.1.0"] = {
-        "There's now a changelog directly within the UI!"
+        "There's now a changelog accessible from directly within the UI!",
+        "Unfortunately, it has to be printed :p"
     },
     ["Version 1.0.0"] = {
         "The script has been created!"
