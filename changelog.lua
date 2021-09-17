be7f8d833d4d64152880b3c29f57912b39910248
Triantafyllo <jessicafra001@gmail.com>
2021-09-17 01:08:23 +0100

    Updated Changelog

--- changelog.lua
+++ changelog.lua
@@ -1,4 +1,10 @@
 local changelog_table = {
+    ["Version 1.2.0"] = {
+        "Addedd Iris Compatibility Script to the UI",
+        "Updated Anti-AFK tick method to not rejoin you anymore, nor make your game become \"TouchEnabled\"",
+        "Optimized code by cleaning up a few things",
+        "Added a rejoin time to the kick message"
+    },
     ["Version 1.1.0"] = {
         "There's now a changelog accessible from directly within the UI!",
         "Unfortunately, it has to be printed :p"
