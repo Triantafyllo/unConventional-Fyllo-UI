0938cba1e55e7382b3de711d0808fe5ed817cf15
Triantafyllo <jessicafra001@gmail.com>
2021-09-17 10:07:23 +0100

    Fixed a bug where the LocalPlayer wouldn't be present and the UI would crash attempting to use GetMouse

--- changelog.lua
+++ changelog.lua
@@ -1,16 +1,19 @@
 local changelog_table = {
+    ["Version 1.2.1"] = {
+        "* Fixed a bug where the script would occasionally error"
+    },
     ["Version 1.2.0"] = {
-        "Addedd Iris Compatibility Script to the UI",
-        "Updated Anti-AFK tick method to not rejoin you anymore, nor make your game become \"TouchEnabled\"",
-        "Optimized code by cleaning up a few things",
-        "Added a rejoin time to the kick message"
+        "+ Addedd Iris Compatibility Script to the UI",
+        "+ Updated Anti-AFK tick method to not rejoin you anymore, nor make your game become \"TouchEnabled\"",
+        "* Optimized code by cleaning up a few things",
+        "+ Added a rejoin time to the kick message"
     },
     ["Version 1.1.0"] = {
-        "There's now a changelog accessible from directly within the UI!",
+        "+ There's now a changelog accessible from directly within the UI!",
         "Unfortunately, it has to be printed :p"
     },
     ["Version 1.0.0"] = {
-        "The script has been created!"
+        "+ The script has been created!"
     }
 }
 
